530fb164e629d380197f85b86218a52601a766a3
LegitOscar <oscarboerjesen@gmail.com>
2026-01-08 01:18:00 +0100

    workflow.yml fix

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,12 +1,13 @@
 package dat;
 
 //import dat.routes.Routes;
-
+//Test
 
 import dat.config.ApplicationConfig;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationConfig.startServer(7171);}
+        ApplicationConfig.startServer(7070);}
 }
+
